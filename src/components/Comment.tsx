1f5fa90212e2d76ce87606414c41ad972f34dd93
mateusgapski <mateusgapski@hubii.com.br>
2022-10-27 08:28:25 -0300

    feat: Updated whole application with typescript

---
+++ src/components/Comment.tsx
@@ -0,0 +1,50 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    setLikeCount (likeCount + 1)
+  } 
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/gapskii.png" alt=''/>
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mateus Gapski</strong>
+              <time title='10 de Outubro às 10:15h' dateTime='2022-10-10 10:15:30'>Publicado há 1h</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          {content}
+        </div>
+
+        <footer>
+          <button type='submit' onClick={handleLikeComment}>
+            <ThumbsUp /> 
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
